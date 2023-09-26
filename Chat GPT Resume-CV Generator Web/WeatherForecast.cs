ae061b39dc69a3be75930fc42f8f14f6380b1906
alphapixel1 <alphapixel2@gmail.com>
2023-09-26 00:19:05 -0400

    Add project files.

---
+++ Chat GPT Resume-CV Generator Web/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Chat_GPT_Resume_CV_Generator_Web
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
