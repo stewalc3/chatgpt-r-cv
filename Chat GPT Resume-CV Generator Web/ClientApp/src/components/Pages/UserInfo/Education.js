350a9abf2940cb6e654b50f7cbfd462a2260301f
alphapixel1 <alphapixel2@gmail.com>
2023-10-04 15:28:22 -0400

    last changes

---
+++ Chat GPT Resume-CV Generator Web/ClientApp/src/components/Pages/UserInfo/Education.js
@@ -0,0 +1,26 @@
+﻿import { CardHeader } from '@mui/material';
+import Card from '@mui/material/Card';
+import IconButton from '@mui/material/IconButton';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+function Education() {
+    return (
+        <Card>
+            <CardHeader action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon/>
+          </IconButton>
+        }/>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    test
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    This is a simple Material-UI card.
+                </Typography>
+            </CardContent>
+        </Card>
+        );
+}
+export default Education;
--- Chat GPT Resume-CV Generator Web/ClientApp/src/components/Pages/UserInfo/UserInfo.js
+++ Chat GPT Resume-CV Generator Web/ClientApp/src/components/Pages/UserInfo/UserInfo.js
@@ -1,6 +1,8 @@
 ﻿import { Box } from '@mui/material';
 import TextField from '@mui/material/TextField';
-
+import Education from "./Education"
+import "./UserInfo.css";
+import AddIcon from '@mui/icons-material/Add';
 export default function UserInfo(){
     return (
         <div>
@@ -14,7 +16,19 @@ export default function UserInfo(){
                 <TextField id="cityInput" label="City" variant="outlined" />
                 <TextField id="stateInput" label="State" variant="outlined" />
                 <TextField id="zipInput" label="Zip" variant="outlined" />
-                
+            </Box>
+            <div className='row edu-proj'>
+                <div className='col card'>
+                    
+                        <AddIcon/>
+                    
+                </div>
+                <div className='col'>
+
+                </div>
+            </div>
+            <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+                <Education/>
             </Box>
 </div>
         );
